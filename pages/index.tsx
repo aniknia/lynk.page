3ca7bd68af2f717407a87c43d8f7d966825d0092
aniknia <aniknia.dev@gmail.com>
2022-11-29 01:12:19 -0800

    refactor: compartmentalize pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,5 @@
-import { Flex } from "@chakra-ui/react";
-import LinkList from "../components/profile/linklist";
-import Profile from "../components/profile/profile";
+import HOME from "../components/home/home";
 
 export default function Home() {
-  return (
-    <>
-      <Flex
-        direction="column"
-        minH="calc(100vh - 40px)"
-        maxW="650px"
-        margin="auto"
-        align="center"
-      >
-        <Profile name="@Profile" alt="Profile Photo" image="/logo.svg" />
-        <LinkList
-          links={[
-            { name: "Welcome to Lynk Page", address: "", internal: true },
-            { name: "To get started you can", address: "", internal: true },
-            {
-              name: "Create your Lynk Page",
-              address: "/create",
-              internal: true,
-            },
-            {
-              name: "Log in to your Lynk Page",
-              address: "/login",
-              internal: true,
-            },
-            {
-              name: "Learn More",
-              address: "/learnmore",
-              internal: true,
-            },
-          ]}
-        />
-      </Flex>
-    </>
-  );
+  return <HOME />;
 }
